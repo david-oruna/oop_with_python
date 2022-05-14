b1ee04d49372efb0e0e044a0dba66d0087969f96
David Hawk <104524650+david-oruna@users.noreply.github.com>
2022-05-13 21:02:29 -0500

    Update 05-init_constructor-2.py

--- 02-init_constructor/05-init_constructor-2.py
+++ 02-init_constructor/05-init_constructor-2.py
@@ -5,20 +5,31 @@
 # We add a test in the __init__() constructor to check
 # if 'value' is an int or not.
 
+#david-oruna: I changed and added some input functions, so it is more customizable :)
 
-class MyNum(object):
-    def __init__(self, value):
+
+class MyNum:
+    def __init__(self, value, message, favorite):
         try:
             value = int(value)
+            print(f'Message is {message}')
+            print(f'Is favorite: {favorite}')
         except ValueError:
             value = 0
         self.value = value
+        self.message = message
+        self.favorite = favorite
 
     def increment(self):
-        self.value = self.value + 1
-        print(self.value)
+        try:
+            addnum = int(input('Enter number to add: '))
+            self.value = self.value + addnum
+            print(f'Your new number is {self.value}')
+        except ValueError as e:
+            print(f'Value error: {e}')
 
 
-a = MyNum(10)
-a.increment()  # This should print 11
-a.increment()  # This should print 12
+a = MyNum(8, 'Hello', True)
+
+a.increment()  # Prints your num + input
+a.increment()  # Prints again your num + input
